094fcbb778fef1a180d7ea868473f90dff8078ed
spaceC00kie <kirstie317@gmail.com>
2023-02-16 14:37:54 -0500

    -main tile filled out

--- src/components/tiles/form/MainTile.tsx
+++ src/components/tiles/form/MainTile.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion"
+import { default as calendar } from "/src/assets/undraw_chore_list.svg"
 
 export const MainTile = () => {
   return (
@@ -9,10 +10,21 @@ export const MainTile = () => {
       transition={{
         duration: 0.6,
       }}
-      className="h-[32em] shrink-0 overflow-hidden rounded-md border border-yellow-700 bg-red-900 bg-opacity-50"
+      className="flex h-[32em] shrink-0 flex-col gap-2 overflow-clip rounded-md border border-yellow-600 bg-red-900 bg-opacity-50 p-2 sm:flex-row"
       style={{
         backdropFilter: "blur(16px)",
       }}
-    ></motion.div>
+    >
+      <div className="grid h-full place-content-center p-2 sm:w-1/2">
+        <div className="rounded-md border border-yellow-600 bg-red-800 bg-opacity-50 p-4 text-lg font-bold text-yellow-50">
+          Just log in with Google, check the AM or PM box when you feed your
+          dog, and come back any time to see the feeding history. Even if
+          someone else checked the box!
+        </div>
+      </div>
+      <div className="grid place-content-center sm:w-1/2">
+        <img src={calendar} alt="calendar" />
+      </div>
+    </motion.div>
   )
 }
