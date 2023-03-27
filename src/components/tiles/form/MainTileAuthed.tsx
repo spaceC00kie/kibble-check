c857ef05d637dc7f0137c36e4e249fee61ade031
spaceC00kie <kirstie317@gmail.com>
2023-03-26 21:47:04 -0400

    carousel prototyped

--- src/components/tiles/form/MainTileAuthed.tsx
+++ src/components/tiles/form/MainTileAuthed.tsx
@@ -1,14 +1,46 @@
+import { motion } from "framer-motion"
 import { DayTile } from "./DayTile"
 
-export const MainTileAuthed = () => {
-  const tiles = Array.from({ length: 9 }).map((_, index) => (
+export const MainTileAuthed: React.FC = () => {
+  const tiles = Array.from({ length: 7 }).map((_, index) => (
     <DayTile key={index} />
   ))
 
+  const selectedTileIndex = Math.floor(tiles.length / 2)
+
   return (
-    <div className="flex h-[36em] shrink-0 flex-col place-content-center gap-2 overflow-hidden rounded-md border border-yellow-600 bg-red-900 bg-opacity-50 p-2 sm:flex-row">
+    <div className="flex h-[36em] shrink-0 flex-col place-content-center overflow-hidden rounded-md border border-yellow-600 bg-red-900 bg-opacity-50 p-2 sm:flex-row">
       <div className="container grid h-full place-content-center overflow-y-auto p-2 sm:w-1/2">
-        <div className="grid h-full gap-2 p-2">{tiles}</div>
+        <div className="-gap-2 grid h-full p-2">
+          {tiles.map((tile, index) => {
+            const distanceFromSelected = Math.abs(index - selectedTileIndex)
+            const scale = 1 - distanceFromSelected / 8
+            const opacity = 1 - distanceFromSelected / 4
+            const zIndex = tiles.length - distanceFromSelected
+            const translateYDirection = index < selectedTileIndex ? 1 : -1
+            const translateY =
+              distanceFromSelected *
+              10 *
+              translateYDirection *
+              (distanceFromSelected * 1.3)
+            const rotateZ = distanceFromSelected * 6
+
+            return (
+              <motion.div
+                layout
+                key={index}
+                className=""
+                style={{
+                  transform: `scale(${scale}) translateY(${translateY}px) rotateZ(${rotateZ}deg)`,
+                  opacity: opacity,
+                  zIndex: zIndex,
+                }}
+              >
+                {tile}
+              </motion.div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
