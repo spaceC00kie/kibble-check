d7029ac32a06845a43da01d15495a69d43ecd890
spaceC00kie <kirstie317@gmail.com>
2023-03-31 16:00:22 -0400

    tiles array now slice of larger array

--- src/components/tiles/form/MainTileAuthed.tsx
+++ src/components/tiles/form/MainTileAuthed.tsx
@@ -3,12 +3,18 @@ import { motion } from "framer-motion"
 import { DayTile } from "./DayTile"
 
 export const MainTileAuthed: React.FC = () => {
-  const tiles = Array.from({ length: 7 }).map((_, index) => (
+  const totalTiles = 47
+  const visibleTiles = 7
+  const allTiles = Array.from({ length: totalTiles }).map((_, index) => (
     <DayTile key={index} index={index} />
   ))
+  const startIndex = Math.floor((totalTiles - visibleTiles) / 2)
+  const endIndex = startIndex + visibleTiles
+
+  const tiles = allTiles.slice(startIndex, endIndex)
 
   const [selectedTileIndex, setSelectedTileIndex] = useState(
-    Math.floor(tiles.length / 2),
+    Math.floor(visibleTiles / 2),
   )
 
   const [scrollValue, setScrollValue] = useState(0)
@@ -43,10 +49,7 @@ export const MainTileAuthed: React.FC = () => {
   return (
     <div className="flex h-[36em] shrink-0 flex-col place-content-center overflow-hidden rounded-md border border-yellow-600 bg-red-900 bg-opacity-50 p-2 sm:flex-row">
       <div className="container grid h-full place-content-center overflow-y-auto p-2 sm:w-1/2">
-        <div
-          id="grid-container"
-          className="relative h-full p-2"
-        >
+        <div id="grid-container" className="relative h-full p-2">
           <div className="-gap-2 grid h-full p-2">
             {tiles.map((tile, index) => {
               const distanceFromSelected = Math.abs(index - selectedTileIndex)
