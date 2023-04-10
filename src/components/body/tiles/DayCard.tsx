747477097045a1337edb7a2e2d0e98c47949b06e
spaceC00kie <kirstie317@gmail.com>
2023-04-10 15:39:37 -0400

    - added dayJS - DayCard displays accurate dates

--- src/components/body/tiles/DayCard.tsx
+++ src/components/body/tiles/DayCard.tsx
@@ -1,11 +1,18 @@
+import dayjs from "dayjs"
+
 interface Props {
   day: number
 }
 
 export const DayCard: React.FC<Props> = ({ day }) => {
+  const todaysDate = dayjs()
+  const dateOffsetValue = day - 50
+  const offsetTileDate = todaysDate.add(dateOffsetValue, "day")
+  const prettyDate = offsetTileDate.format("MMMM D, YYYY")
+
   return (
-    <div className="w-96 place-content-center rounded-md border border-yellow-600 bg-red-900 p-6 text-2xl font-semibold text-yellow-50">
-      Date:___{day}___ AM__ PM__
+    <div className="w-96 whitespace-nowrap overflow-clip place-content-center rounded-md border border-yellow-600 bg-red-900 p-6 text-2xl font-semibold text-yellow-50">
+      {prettyDate}: __ AM __ PM
     </div>
   )
 }
