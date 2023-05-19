c214075fa562e6b8bf84526a8e9e21142a449421
spaceC00kie <kirstie317@gmail.com>
2023-05-18 22:40:17 -0400

    - added discord to footer, removed Support - Donate popup now a toast

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,11 +2,10 @@ import { motion } from "framer-motion"
 import React from "react"
 import { AiOutlineGithub } from "react-icons/ai"
 import { BiSupport } from "react-icons/bi"
-import { FaDonate } from "react-icons/fa"
+import { FaDonate, FaDiscord } from "react-icons/fa"
 import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
 import { Donate } from "../popups/Donate"
-import { CustomerSupport } from "../popups/Support"
 import { SocialLink } from "./SocialLink"
 
 const MySwal = withReactContent(Swal)
@@ -16,33 +15,24 @@ interface Props {}
 export const Footer: React.FC<Props> = ({}) => {
   return (
     <motion.div className="flex h-full flex-col justify-end">
-      <div className="flex h-14 w-full items-center justify-between px-24">
+      <div className="flex h-14 w-full items-center justify-between">
         <SocialLink
           href="https://github.com/spaceC00kie/kibble-check"
           icon={<AiOutlineGithub />}
           title="Source Code"
         />
         <SocialLink
-          title="Contact"
-          icon={<BiSupport />}
-          onClick={() => {
-            MySwal.fire({
-              html: <CustomerSupport />,
-              icon: "warning",
-              color: "#b45309",
-              background: "#fefce8",
-              confirmButtonText: "Close",
-              confirmButtonColor: "#b45309",
-            })
-          }}
-        />{" "}
+          title="Discord"
+          href="https://discord.gg/zGbx9x6YWw"
+          icon={<FaDiscord />}
+        />
         <SocialLink
           title="Donate"
           icon={<FaDonate />}
           onClick={() => {
             MySwal.fire({
               html: <Donate />,
-              icon: "info",
+              toast: true,
               color: "#b45309",
               background: "#fefce8",
               confirmButtonText: "Close",
